d99e588a826f1bb9da5d7e1ac91bde2eaaa8f310
Merouane Bellaha <m.bellaha@gmail.com>
2020-06-27 10:36:46 +0200

    same

--- CoreDataManager/FakeCoreDataStack.swift
+++ CoreDataManager/FakeCoreDataStack.swift
@@ -1,9 +1,34 @@
+////
+////  FakeCoreDataStack.swift
+////  CoreDataManager
+////
+////  Created by Merouane Bellaha on 25/06/2020.
+////  Copyright © 2020 Merouane Bellaha. All rights reserved.
+////
 //
-//  FakeCoreDataStack.swift
-//  CoreDataManager
+//import TDL
+//import Foundation
+//import CoreData
 //
-//  Created by Merouane Bellaha on 25/06/2020.
-//  Copyright © 2020 Merouane Bellaha. All rights reserved.
+//final class FakeCoreDataStack: CoreDataStack {
 //
-
-import Foundation
+//    // MARK: - Initializer
+//
+//    convenience init() {
+//        self.init(containerName: "ToDoList")
+//    }
+//
+//    override init(containerName : String) {
+//        super.init(containerName: containerName)
+//        let persistentStoreDescription = NSPersistentStoreDescription()
+//        persistentStoreDescription.type = NSInMemoryStoreType
+//        let container = NSPersistentContainer(name: containerName)
+//        container.persistentStoreDescriptions = [persistentStoreDescription]
+//        container.loadPersistentStores { storeDescription, error in
+//            if let error = error as NSError? {
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
+//            }
+//        }
+//        self.persistentContainer = container
+//    }
+//}
