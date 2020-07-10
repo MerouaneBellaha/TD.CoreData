6b8b0747101c8c652340f0b911f7febb931f335e
Merouane Bellaha <m.bellaha@gmail.com>
2020-07-10 12:45:58 +0200

    added CoreData Unit Test

--- CoreDataManager/CoreDataManagerTests.swift
+++ CoreDataManager/CoreDataManagerTests.swift
@@ -1,54 +1,58 @@
-////
-////  CoreDataManager.swift
-////  CoreDataManager
-////
-////  Created by Merouane Bellaha on 25/06/2020.
-////  Copyright © 2020 Merouane Bellaha. All rights reserved.
-////
 //
+//  CoreDataManager.swift
+//  CoreDataManager
 //
-//@testable import TDL
-//import XCTest
+//  Created by Merouane Bellaha on 25/06/2020.
+//  Copyright © 2020 Merouane Bellaha. All rights reserved.
 //
-//class CoreDataManagerTests: XCTestCase {
-//
-//    var coreDataStack: FakeCoreDataStack!
-//    var coreDataManager: CoreDataManager!
-//
-//    override func setUp() {
-//        super.setUp()
-//        coreDataStack = FakeCoreDataStack()
-//        coreDataManager = CoreDataManager(with: coreDataStack)
-//    }
-//
-//    override func tearDown() {
-//        super.tearDown()
-//        coreDataStack = nil
-//        coreDataManager = nil
-//    }
-//
-//    func testGivenCoreDataManagerIsEmpty_WhenCreateAnItem_ThenCoreDataManagerShouldHaveOneTask() {
-//        coreDataManager.createItem(named: "A task")
-//        print(coreDataManager.loadItems().count)
-//        XCTAssertTrue(coreDataManager.loadItems().count == 1)
-//
-//
-//    }
-//
-////    func testGiven<#Given#>_When<#When#>_Then<#Then#>() {
-////    <#code#>
-////    }
-//
-//    func testExample() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-//
-//}
+
+
+@testable import TDL
+import XCTest
+
+class CoreDataManagerTests: XCTestCase {
+
+    var coreDataStack: FakeCoreDataStack!
+    var coreDataManager: CoreDataManager!
+
+    override func setUp() {
+        super.setUp()
+        coreDataStack = FakeCoreDataStack()
+        coreDataManager = CoreDataManager(with: coreDataStack)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        coreDataStack = nil
+        coreDataManager = nil
+    }
+
+    func testGivenCoreDataManagerIsEmpty_WhenCreateAnItem_ThenCoreDataManagerShouldHaveOneTaskNamedATask() {
+        coreDataManager.createItem(named: "A task")
+
+        XCTAssertTrue(coreDataManager.loadItems().count == 1)
+        XCTAssertTrue(coreDataManager.loadItems().first?.taskName == "A task")
+    }
+
+    func testGivenCoreDataManagerIsNotEmpty_WhenDeleteAllTasks_ThenCoreDataManagerShouldBeEmpty() {
+        coreDataManager.createItem(named: "A task")
+
+        coreDataManager.deleteItems()
+
+        XCTAssertTrue(coreDataManager.loadItems().isEmpty)
+
+    }
+
+    func testGivenCoreDataManagerContainsTwoTasks_WhenDeleteTheFirstTask_ThenCoreDataManagerShouldContainsOnlyTheSecondTask() {
+        coreDataManager.createItem(named: "First Task")
+        coreDataManager.createItem(named: "Second Task")
+
+        guard let firstTask = coreDataManager.loadItems(containing: "First Task").first else { return }
+
+        coreDataManager.deleteItem(item: firstTask)
+
+        XCTAssertTrue(coreDataManager.loadItems().count == 1)
+        XCTAssertTrue(coreDataManager.loadItems().first?.taskName == "Second Task")
+
+    }
+}
--- CoreDataManager/FakeCoreDataStack.swift
+++ CoreDataManager/FakeCoreDataStack.swift
@@ -1,34 +1,34 @@
-////
-////  FakeCoreDataStack.swift
-////  CoreDataManager
-////
-////  Created by Merouane Bellaha on 25/06/2020.
-////  Copyright © 2020 Merouane Bellaha. All rights reserved.
-////
 //
-//import TDL
-//import Foundation
-//import CoreData
+//  FakeCoreDataStack.swift
+//  CoreDataManager
 //
-//final class FakeCoreDataStack: CoreDataStack {
+//  Created by Merouane Bellaha on 25/06/2020.
+//  Copyright © 2020 Merouane Bellaha. All rights reserved.
 //
-//    // MARK: - Initializer
-//
-//    convenience init() {
-//        self.init(containerName: "ToDoList")
-//    }
-//
-//    override init(containerName : String) {
-//        super.init(containerName: containerName)
-//        let persistentStoreDescription = NSPersistentStoreDescription()
-//        persistentStoreDescription.type = NSInMemoryStoreType
-//        let container = NSPersistentContainer(name: containerName)
-//        container.persistentStoreDescriptions = [persistentStoreDescription]
-//        container.loadPersistentStores { storeDescription, error in
-//            if let error = error as NSError? {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        }
-//        self.persistentContainer = container
-//    }
-//}
+
+import TDL
+import Foundation
+import CoreData
+
+final class FakeCoreDataStack: CoreDataStack {
+
+    // MARK: - Initializer
+
+    convenience init() {
+        self.init(containerName: "TaskModel")
+    }
+
+    override init(containerName : String) {
+        super.init(containerName: containerName)
+        let persistentStoreDescription = NSPersistentStoreDescription()
+        persistentStoreDescription.type = NSInMemoryStoreType
+        let container = NSPersistentContainer(name: containerName)
+        container.persistentStoreDescriptions = [persistentStoreDescription]
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        self.persistentContainer = container
+    }
+}
